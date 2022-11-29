4c712ace26264e849a2b755c2fbca2c2682e1956
justinklee345 <72401945+justinklee345@users.noreply.github.com>
2022-11-29 20:53:52 +0000

    Added tests

--- src/graph.cpp
+++ src/graph.cpp
@@ -1,16 +1,18 @@
 #include "graph.h"
+#include <bits/stdc++.h>
 
 // populate adjacency matrix with dataset
 Graph::Graph(const string& filename) {
     // create file input stream
     ifstream input(filename);
 
-    // ensure file is open 
+    // ensure file is open
     if (input.is_open()) {
         // read in lines from file
         string line;
         // get column names
         getline(input, line);
+        cout << "Got to here!" << endl;
         int SOURCE, TARGET, RATING, TIME;
         while (getline(input, line)) {
             // create line string stream
@@ -23,22 +25,22 @@ Graph::Graph(const string& filename) {
             str_stream.ignore();
             str_stream >> TIME;
             // put rating in adjacency matrix
-            adj_matrix[SOURCE][RATING] = RATING;
+            adj_matrix[SOURCE][TARGET] = RATING;
+            cout << adj_matrix[SOURCE][TARGET] << endl;
         }
     }
     input.close();
 }
-
 
 int Graph::dijkstra(int src, int target) const {
     vector<int> distance(adj_matrix.size(), INT_MAX);
     vector<bool> incShort(adj_matrix.size(), false);
 
     distance[src] = 0;
-    for (int i = 0; i < adj_matrix[0].size() - 1; ++i) {
+    for (size_t i = 0; i < adj_matrix[0].size() - 1; ++i) {
         int mindist = minDistance(distance, incShort);
         incShort[mindist] = true;
-        for (int node = 0; node < adj_matrix[0].size(); ++node) {
+        for (size_t node = 0; node < adj_matrix[0].size(); ++node) {
             if (!incShort.at(node) && adj_matrix[mindist][node] != 0) {
                 if (distance.at(mindist) != INT_MAX && distance.at(mindist) + adj_matrix[mindist][node] < distance.at(node)) {
                     distance[node] = distance[mindist] + adj_matrix[mindist][node];
@@ -52,11 +54,15 @@ int Graph::dijkstra(int src, int target) const {
 int Graph::minDistance(vector<int> distance, vector<bool> incShort) const {
     int min = INT_MAX;
     int min_idx = -1;
-    for (int node = 0; node < adj_matrix[0].size(); ++node) {
+    for (size_t node = 0; node < adj_matrix[0].size(); ++node) {
         if (!incShort.at(node) && distance.at(node) <= min) {
             min = distance.at(node);
             min_idx = node;
         }
     }
     return min_idx;
+}
+
+const vector<vector<int>>& Graph::getMatrix() const {
+    return adj_matrix;
 }
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,4 +1,19 @@
 #include <catch2/catch_test_macros.hpp>
+#include "../src/graph.h"
 
-TEST_CASE("file_to_V2D Public Test 1", "[weight=5]") {
+TEST_CASE("Constructor Test for smaller file", "[weight=5]") {
+    Graph testing("constructorTest.csv");
+    vector<vector<int>> matrix = testing.getMatrix();
+    cout << "Printing Matrix: " << endl;
+    for (size_t i = 0; i < matrix.size(); ++i) {
+        for (size_t j = 0; j < matrix.at(i).size(); ++j) {
+            cout << matrix[i][j] << "; ";
+        }
+        cout << endl;
+    }
+    REQUIRE(matrix[2][1] == 9);
+    REQUIRE(matrix[3][0] == 8);
+    REQUIRE(matrix[1][3] == 9);
+    REQUIRE(matrix[0][2] == 9);
+    REQUIRE(true);
 }
