ac243a8f76d8413ecff5238426114d02161a382c
danielkoexe <danielko.exe@gmail.com>
2022-11-28 23:36:41 -0600

    rewrote constructor and sorted data

--- src/graph.h
+++ src/graph.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <vector>
 #include <cstring>
 #include <map>
@@ -9,18 +10,13 @@ using namespace std;
 class Graph {
   	public:
     	Graph(const string& filename);
-		void addEdge(string from, string to, int weight);
-		int dijkstra(string src, string target) const;
+		int dijkstra(int src, int target) const;
 	private:
-		void populateMatrix(vector<pair<pair<string, string>, int>> edges);
 		int minDistance(vector<int> distance, vector<bool> incShort) const;
-		int get_id(string src) const;
-		map<string, int> nodeID_map;
 
 		/*
 		each element in the adj_matrix tells us only one direction...
 		ex) if adj_matrix[0][1] == 1, vertex 0 follows vertex 1 (0 --> 1), but not the other way around
 		*/
 		vector<vector<int>> adj_matrix;
-		int num_nodes_;
 };
