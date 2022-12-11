ea2a4e355ae09c3968129ddfbdfb35fc4ea9f9c3
justinklee345 <72401945+justinklee345@users.noreply.github.com>
2022-12-11 20:41:07 +0000

    Added constructor tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,19 +1,36 @@
 #include <catch2/catch_test_macros.hpp>
 #include "../src/graph.h"
 
-TEST_CASE("Constructor Test for smaller file", "[weight=5]") {
-    Graph testing("constructorTest.csv");
-    vector<vector<int>> matrix = testing.getMatrix();
-    cout << "Printing Matrix: " << endl;
-    for (size_t i = 0; i < matrix.size(); ++i) {
-        for (size_t j = 0; j < matrix.at(i).size(); ++j) {
-            cout << matrix[i][j] << "; ";
-        }
-        cout << endl;
-    }
-    REQUIRE(matrix[2][1] == 9);
-    REQUIRE(matrix[3][0] == 8);
-    REQUIRE(matrix[1][3] == 9);
-    REQUIRE(matrix[0][2] == 9);
-    REQUIRE(true);
+TEST_CASE("Constructor Test (SMALL) normal (randomly selected) test", "[weight=5]") {
+    Graph testing("../tests/constructorTest.csv");
+
+    REQUIRE(testing.getRating(3, 2) == 9);
+    REQUIRE(testing.getRating(4, 1) == 8);
+    REQUIRE(testing.getRating(1, 3) == 8);
 }
+
+TEST_CASE("Constructor Test (SMALL) duplicates test", "[weight=5]") {
+    Graph testing("../tests/constructorTest.csv");
+
+    // In the case of duplicate ratings (src rates target after two or more separate transactions),
+    // we will discard all previous ratings and update our adjacency matrix with most recent rating.
+    REQUIRE(testing.getRating(2, 4) == 5);
+    REQUIRE(testing.getRating(4, 1) == 8);
+}
+
+TEST_CASE("Constructor Test (ACTUAL) normal (randomly selected) test", "[weight=5]") {
+    Graph testing("../data/sorted-soc-sign-bitcoinalpha.csv");
+
+    REQUIRE(testing.getRating(15, 3451) == 1);
+    REQUIRE(testing.getRating(3451, 98) == 5);
+    REQUIRE(testing.getRating(114, 7370) == -1);
+}
+
+// TEST_CASE("Constructor Test (ACTUAL) duplicates test", "[weight=5]") {
+//     Graph testing("../data/sorted-soc-sign-bitcoinalpha.csv");
+
+//     // In the case of duplicate ratings (src rates target after two or more separate transactions),
+//     // we will discard all previous ratings and update our adjacency matrix with most recent rating.
+//     REQUIRE(testing.getRating(2, 4) == 5);
+//     REQUIRE(testing.getRating(4, 1) == 8);
+// }
